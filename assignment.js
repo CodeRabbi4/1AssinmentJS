3bd56be50734b8745710e220fec0a506c21e46a4
rabbi2004 <rrrrr08102004@gmail.com>
2023-06-02 21:35:32 +0600

    1Assinment

---
+++ assignment.js
@@ -0,0 +1,74 @@
+// ১. Variable কি ?
+
+Answar:Variable হলো এমন একটি প্রাত্র যেখানে তথ্য সংরক্ষন করে রাখা হয় ।
+
+// ২. Variable কিভাবে লিখতে হয় ?
+Answer: var myName = "Rabbi Hasan";
+       console.log(myName);
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+Answar:""ডাবল কোট বা ' ' সিঙ্গেল কোটের ভিতরে যা লেখা হয় ভ্যারিয়বেলের ভ্যালু তাকে স্ট্রিং ডাটা টাইপ (string data type) বলা হয় ।
+ স্ট্রিং এর অর্থ টেক্সট ।
+
+ var stiType = "Hello World";
+ console.log(stiType);
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+Answer: Number type Variable গানিতিক সংখ্যায় লেখা হয় ।
+
+var numtype = 30;
+console.log(numtype);
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+Answer: Boolan type Variable  true এবং false দুইটি ভ্যালু দিয়ে লিখতে হয় ।
+
+var booltype = true;
+console.log(booltype);
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+Answer:
+var upper = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+console.log(upper.toUpperCase());
+
+var lower = "LOREM IPSUM IS SIMPLY DUMMY TEXT OF THE PRINTING AND TYPESETTING INDUSTRY. LOREM IPSUM HAS BEEN THE INDUSTRY'S STANDARD DUMMY TEXT EVER SINCE THE 1500S, WHEN AN UNKNOWN PRINTER TOOK A GALLEY OF TYPE AND SCRAMBLED IT TO MAKE A TYPE SPECIMEN BOOK."
+"LOREM IPSUM IS SIMPLY DUMMY TEXT OF THE PRINTING AND TYPESETTING INDUSTRY. LOREM IPSUM HAS BEEN THE INDUSTRY'S STANDARD DUMMY TEXT EVER SINCE THE 1500S, WHEN AN UNKNOWN PRINTER TOOK A GALLEY OF TYPE AND SCRAMBLED IT TO MAKE A TYPE SPECIMEN BOOK.";
+console.log(lower.toLowerCase());
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+Answer: JavaaScript এর ২ টি অপারেটর আছে ।
+১। গানিতিক অপারেটর।
+২। অ্যাসাইনমেন্ট অপারেটর।
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+Answer:
+var absolute = -50;
+var sum = Math.abs(absolute);
+console.log(sum);
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+Answer:
+var ceil = 0.001;
+var sum = Math.ceil(ceil);
+console.log(sum);
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+Answer:
+var floor = 0.8;
+var sum = Math.floor(floor);
+console.log(sum);
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+Answer:
+var roun = 0.5;
+var sum = Math.round(roun);
+console.log(sum);
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+Answer:
+var rand = Math.random();
+console.log(rand);
+ 
+// asignment.js - GitHub Link 
+
+https://github.com/rabbi2004/1AssinmentJS
